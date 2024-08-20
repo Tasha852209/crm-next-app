4b0fe8934c0a9211676be30d2742e37d0c12008f
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2024-08-20 23:17:12 +0300

    added styles

---
+++ src/app/components/status-label.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export enum Status {
+  Active = 'active',
+  NotActive = 'notActive',
+  Pending = 'pending',
+  Suspended = 'suspended',
+}
+export interface StatusLabelProps {
+  children: React.ReactNode;
+  status: Status;
+}
+
+export default function StatusLabel({ children, status }: StatusLabelProps) {
+  return (
+    <div
+      className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${(status === Status.Active && 'text-red-700 bg-red-100') || (status === Status.NotActive && 'text-orange-700 bg-orange-100') || (status === Status.Pending && 'text-blue-700 bg-blue-100') || (status === Status.Suspended && 'text-green-700 bg-green-100') || ''}`}
+    >
+      <div className="w-1 h-1 mr-2 rounded-full bg-current"></div>
+      {children}
+    </div>
+  );
+}
